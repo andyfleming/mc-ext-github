02cbea9702003435bcb66cd96e38b8b4c0a9cc75
Andy Fleming <andy@andyfleming.com>
2016-02-06 19:48:50 -0800

    Starting pipeline events file

---
+++ src/pipeline-events/status-api.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = {
+
+  /**
+   * When a pipeline starts, if it was triggered by a webhook, we'll set a status for it
+   */
+  start: () => {
+    
+  },
+
+  /**
+   * When a pipeline ends, if it was triggered by a webhook, we'll update its status
+   */
+  complete: () => {
+
+  }
+
+}
