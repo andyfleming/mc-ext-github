251504d445d9d08febe8b74ae65e6dd01225d19c
Andy Fleming <andy@andyfleming.com>
2016-02-11 01:48:21 -0800

    starting on implementation of status api settings

--- src/pipeline-settings/status-api-settings.js
+++ src/pipeline-settings/status-api-settings.js
@@ -2,6 +2,16 @@
 
 module.exports = {
 
+  id: 'status_api',
+  name: 'GitHub Status API Settings',
 
+  settings: {
+    account: {
+      name: 'GitHub Account',
+      type: 'account',
+      account_type: 'github',
+      required: true
+    }
+  }
 
 }
